6803b75940c8d5d88b7c0fb38641c99eff7568cf
Aaron <arichner@d.umn.edu>
2016-06-09 10:33:08 -0500

    update to custom field format

--- init.rb
+++ init.rb
@@ -22,8 +22,8 @@ module Redmine
       # include ActionView::Helpers::TextHelper
       # include ActionView::Helpers::TagHelper
 
-      # add 'RT'
-      # self.form_partial = 'custom_fields/formats/rt'
+      add 'RT'
+      self.form_partial = 'custom_fields/formats/rt'
 
       # def format_as_rt(value)
       #   if Setting.plugin_redmine_rt_custom_field['new_window'] == "true"
